3dfab9ef3ce22d7f33b351856c4749a706999846
Luiz fernando Cardoso Vilela <luiz@pop-os.localdomain>
2021-10-10 16:51:18 -0300

    Adequar página home

--- views/home/index.php
+++ views/home/index.php
@@ -14,9 +14,7 @@ require_once "../../models/AdministradorModel.php";
     <?php include "../includes/menu.php"; ?>
     <div class="container">
         <h2>System XYZ - Bem vindo</h2>
-        <?php if ($_GET['result']) { ?>
-            <p>Olá <?php echo unserialize($_SESSION["administrador"])->getNome()  ?>, seja bem vindo!</p>
-        <?php } ?>
+        <p>Olá <?php echo unserialize($_SESSION["administrador"])->getNome()  ?>, seja bem vindo!</p>
     </div>
     <?php include "../includes/js.php"; ?>
 </body>
